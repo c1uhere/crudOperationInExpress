895055f22505606e46d202fce4dbedb9fbb89b96
cluthere <69466884+cluthere/@users.noreply.github.com>
2024-01-02 23:33:44 +0530

    connected db to atlas

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const DB_NAME = require("./constant")
+require("dotenv/config");
 
 async function connection(){
     try{
-        await mongoose.connect("mongodb://0.0.0.0:27017/testdb");
+        await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
+        //mongodb+srv://yourAtlasUsernameHere:yourpasswordhere@cluster0.dptkfe3.mongodb.net put it inside varible DB_URI inside .env file
         console.log('db connected');
     } catch(error){
         console.log(`db not connected ${error}`);
